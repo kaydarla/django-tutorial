3ac3ab97c675c4e63b0adb9e4defde7182f24126
Rakesh Kadarla <rakeshk@Z0d>
2012-04-19 10:38:15 +0530

    Addded app, polls.

---
+++ polls/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("publish date")
+
+    def __unicode__(self):
+        return self.question    
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    poll   = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes  = models.IntegerField()
+
+    def __unicode__(self):
+        return self.choice
+        
+        
+
