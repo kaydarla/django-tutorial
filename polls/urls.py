71b73639cb7d7e910f72c13b293d94b9c09e49e5
Rakesh Kadarla <rakesh.kadarla@gmail.com>
2012-04-20 07:47:35 +0530

    part 4; to be continued

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,6 @@ from polls.views import detail, results, vote
 urlpatterns = patterns('polls.views',
     url(r'^$', 'index'),
     url(r'^(?P<poll_id>\d+)/$', 'detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results', name='presults'),
     url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
 )
